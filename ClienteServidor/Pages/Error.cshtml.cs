f7c55ec233d358f8d6751071eb6f5d330bfe4440
xanwerneck <alexandre.andreza@genius.com>
2022-10-09 23:47:27 -0300

    commit inicial para aula de cliente servidor

---
+++ ClienteServidor/Pages/Error.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClienteServidor.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ErrorModel : PageModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    private readonly ILogger<ErrorModel> _logger;
+
+    public ErrorModel(ILogger<ErrorModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        Console.WriteLine(this.HttpContext.Request.Method);
+        Console.WriteLine(this.HttpContext.Request.Path.ToString());
+    }
+}
+
+
---
+++ ClienteServidor/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClienteServidor.Pages;
+
+public class IndexModel : PageModel
+{
+    public string? Method { get; set; }
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        Method = this.HttpContext.Request.Method;
+        Console.WriteLine(this.HttpContext.Request.Method);
+        Console.WriteLine(this.HttpContext.Request.Path.ToString());
+    }
+}
+
