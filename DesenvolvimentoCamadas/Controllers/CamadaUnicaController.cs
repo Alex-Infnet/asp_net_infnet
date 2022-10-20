64b0ce68951fa85f1030007bc3d69ff87b9cbc21
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2022-10-20 15:36:51 -0300

    commit inicial para aula 3 sobre desenvolvimento em camadas

---
+++ DesenvolvimentoCamadas/Controllers/CamadaUnicaController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DesenvolvimentoCamadas.Controllers
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Nota { get; set; }
+        public double Frequencia { get; set; }
+        public string Situacao { get; set; }
+        public Resultado Resultado { get; set; }
+        public Aluno(int _Id, string _Nome, string _Email, string _Situacao, double _Frequencia, double _Nota)
+        {
+            Id = _Id;
+            Nome = _Nome;
+            Email = _Email;
+            Situacao = _Situacao;
+            Frequencia = _Frequencia;
+            Nota = _Nota;
+        }
+    }
+    public class Resultado
+    {
+        public string Status { get; set; }
+    }
+    public class Historico
+    {
+        public string Status { get; set; }
+    }
+    public class CamadaUnicaController : Controller
+    {
+        public List<Aluno> alunos { get; set; }
+        public CamadaUnicaController()
+        {
+            alunos = new List<Aluno>();
+            alunos.Add(new Aluno(1, "Aluno 1", "aluno1@email.com.br", "Matriculado", 0.9, 7.5));
+            alunos.Add(new Aluno(2, "Aluno 2", "aluno2@email.com.br", "Trancado", 0.5, 0));
+            alunos.Add(new Aluno(3, "Aluno 3", "aluno3@email.com.br", "Matriculado", 0.6, 5.5));
+        }
+
+        [HttpGet]
+        public JsonResult CalcularResultado(int Id)
+        {
+            /* - User Story
+             * Como um administrador de Universidade
+             * Eu desejo saber se um aluno foi aprovado ou nao
+             * Para atualizar o historico desse aluno.
+             * 
+             * - Regras
+             * O aluno precisa estar matriculado
+             * Para o aluno nao matriculado deve ser retornado vazio
+             * O aluno para ser aprovado precisa ter frequencia maior ou igual a 75%
+             * O aluno para ser reprovado precisa ter nota superior a 5
+             */
+
+            var aluno = alunos.First(a => a.Id == Id);
+            if (aluno.Situacao == "Matriculado")
+            {
+                return new JsonResult(null);
+            }
+            var resultado = new Resultado();
+            if (aluno.Frequencia < 0.75)
+            {
+                resultado.Status = "Reprovado";
+            }
+            if (aluno.Frequencia < 0.75)
+            {
+                resultado.Status = "Reprovado";
+            }
+            aluno.Resultado = resultado;
+            return new JsonResult(resultado);
+        }
+
+        [HttpGet]
+        public JsonResult CalcularResultadoTodosAlunos()
+        {
+            /*
+             * TO BE IMPLEMENTED
+             */
+            return new JsonResult(null);
+        }
+    }
+}
+
---
+++ DesenvolvimentoCamadas/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesenvolvimentoCamadas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
