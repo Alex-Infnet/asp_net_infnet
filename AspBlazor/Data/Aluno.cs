ee3db3a567d67ba5fb6982af57f48a62cf4e2e2c
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2022-10-25 22:29:19 -0300

    commit inicial contendo conteúdo da aula 4 utilizando Blazor for ASP net pages

---
+++ AspBlazor/Data/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace AspBlazor.Data
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Aluno(int _Id, string _Nome, string _Email)
+        {
+            Id = _Id;
+            Nome = _Nome;
+            Email = _Email;
+        }
+    }
+}
+
---
+++ AspBlazor/Service/AlunoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspBlazor.Data;
+namespace AspBlazor.Service
+{
+    public class AlunoService
+    {
+        List<Aluno> alunos { get; set; }
+        public AlunoService()
+        {
+            alunos = new List<Aluno>();
+            alunos.Add(new Aluno(1, "Nome do aluno1", "aluno1@email.com.br"));
+            alunos.Add(new Aluno(2, "Nome do aluno2", "aluno2@email.com.br"));
+            alunos.Add(new Aluno(3, "Nome do aluno3", "aluno3@email.com.br"));
+        }
+        public Task<List<Aluno>> getAlunos()
+        {   
+            return Task.FromResult<List<Aluno>>(alunos);
+        }
+        public void Remove(int Id)
+        {
+            alunos.Remove(alunos.Find(a => a.Id == Id));
+        }
+    }
+}
+
