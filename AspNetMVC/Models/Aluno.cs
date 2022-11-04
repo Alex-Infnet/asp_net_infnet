dfe7e5e9205acb324566c61543663797d74b3748
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2022-11-03 21:59:33 -0300

    commit com conteudo da aula 6 - aspnet mvc

---
+++ AspNetMVC/Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AspNetMVC.Data;
+
+namespace AspNetMVC.Models
+{
+    public class Aluno
+    {
+        public string? Nome { get; set; }
+        public string? Matricula { get; set; }
+        public string? Situacao { get; set; }
+
+        private DAAluno? DAAluno { get; set; }
+
+        public Aluno()
+        {
+        }
+
+        public List<Aluno> BuscarTodos()
+        {
+            DAAluno = new DAAluno();
+            return DAAluno.alunos;
+        }
+    }
+}
+
