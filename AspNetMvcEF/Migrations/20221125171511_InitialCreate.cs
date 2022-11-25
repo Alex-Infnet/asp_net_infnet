40d69690613f75351265490eea7594bd84f8b403
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2022-11-25 16:08:35 -0300

    commit initial com conteudo da aula 13 de entity framework com linq

---
+++ AspNetMvcEF/Migrations/20221125171511_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetMvcEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sku = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Peso = table.Column<double>(type: "float", nullable: false),
+                    PrecoCusto = table.Column<double>(type: "float", nullable: false),
+                    PrecoVenda = table.Column<double>(type: "float", nullable: false),
+                    Validade = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "produtos");
+        }
+    }
+}
---
+++ AspNetMvcEF/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AspNetMvcEF.Models
+{
+	public class Produto
+	{
+		public int Id { get; set; }
+		public string? Sku { get; set; }
+		public string? Descricao { get; set; }
+		public double Peso { get; set; }
+		public double PrecoCusto { get; set; }
+		public double PrecoVenda { get; set; }
+		public DateTime Validade { get; set; }
+	}
+}
+
