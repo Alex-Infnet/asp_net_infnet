96bd9c29844b13dca781458ea0930d6e1e61b92a
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2022-11-27 11:20:59 -0300

    commit inicial para exercicio de hands on durante a aula 14

---
+++ LojaHandsOn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LojaHandsOn.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ LojaHandsOn/Models/LojaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaHandsOn.Models
+{
+	public class LojaContext : DbContext
+	{
+		public LojaContext(DbContextOptions<LojaContext> options) : base(options)
+		{
+		}
+	}
+}
+
